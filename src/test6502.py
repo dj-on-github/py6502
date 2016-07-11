e7332d3740b572cca59370d93b63c8517bda9c20
David Johnston <dj@deadhat.com>
2016-07-10 21:45:54 -0700

    Changed test6502.py to use a triple quotes assembly listing for better niceness

--- src/test6502.py
+++ src/test6502.py
@@ -5,267 +5,266 @@ import sim6502
 import dis6502
 
 def go(debug=0):  
-    lines = list()
-    lines.append("       org $0000")
-    lines.append("    JMP overthere ")
-    lines.append("bhaddr:    dw &brkhandler,$0102,&another,&labels ; insert address for brkhandler")
-    lines.append("l8addr:    dw &land8")
-    lines.append("overthere:	      ")
-    lines.append("    LDA bhaddr ")
-    lines.append("    LDX 1 ")
-    lines.append("    STA $FFFE ")
-    lines.append("    LDA bhaddr,x ")
-    lines.append("    STA $FFFF ")
-    lines.append("    LDA #$10 ")
-    lines.append("    ADC #$55	    ")
-    lines.append("    ADC $20	        ")
-    lines.append("    ADC $20,X	    ")
-    lines.append("    ADC $0002	        ")
-    lines.append("    ADC $0010,X	    ")
-    lines.append("    ADC $0008,Y	    ")
-    lines.append("    ADC ($20,X)	    ")
-    lines.append("    ADC ($20),Y	    ")
-    lines.append("another:   ADC ($20)	    ")
-    lines.append("    AND #$55	    ")
-    lines.append("    AND $20	        ")
-    lines.append("    AND $20,X	    ")
-    lines.append("    AND $0004	        ")
-    lines.append("labels:	        ")
-    lines.append("    AND $0010,X	    ")
-    lines.append("    AND $0012,Y	    ")
-    lines.append("    AND ($20,X)	    ")
-    lines.append("    AND ($20),Y	    ")
-    lines.append("    AND ($20)	    ")
-    lines.append("    ASL A	        ")
-    lines.append("    ASL $20	        ")
-    lines.append("    ASL $20,X	    ")
-    lines.append("    ASL $2233	        ")
-    lines.append("    ASL $2233,X	    	    ")
-    lines.append("    CLC ")
-    lines.append("    BCC land1	    ")
-    lines.append("    CLC ")
-    lines.append("land1:	        ")
-    lines.append("    SEC ")
-    lines.append("    BCS land2	    ")
-    lines.append("land2:	        ")
-    lines.append("    BEQ land3	    ")
-    lines.append("land3: ")
-    lines.append("    BIT #$55	    ")
-    lines.append("    BIT $20	    ")
-    lines.append("    BIT $20,X	    ")
-    lines.append("    BIT $2233	    ")
-    lines.append("    BIT $2233,X	    ")
-    lines.append("    BMI land4	    ")
-    lines.append("land4: ")
-    lines.append("    BNE land5	    ")
-    lines.append("land5: ")
-    lines.append("    BPL land6	    ")
-    lines.append("land6: ")
-    lines.append("    BRA land7	    ")
-    lines.append("land7: ")
-    lines.append("    LDA l8addr")
-    lines.append("    STA $fffe")
-    lines.append("    LDX #$01")
-    lines.append("    LDA l8addr,x")
-    lines.append("    STA $ffff")
-    lines.append("    BRK	            ")
-    lines.append("    BVC land8	    ")
-    lines.append("land8: ")
-    lines.append("    BVS land9	    ")
-    lines.append("land9: ")
-    lines.append("    CLC	            ")
-    lines.append("    CLD	            ")
-    lines.append("    CLI	            ")
-    lines.append("    CLV	            ")
-    lines.append("    CMP #$55	    ")
-    lines.append("    CMP $20	        ")
-    lines.append("    CMP $20	        ")
-    lines.append("    CMP $2233	        ")
-    lines.append("    CMP $2233,X	    ")
-    lines.append("    CMP $2233,Y	    ")
-    lines.append("    CMP ($20,X)	    ")
-    lines.append("    CMP ($20),Y	    ")
-    lines.append("    CMP ($20)	    ")
-    lines.append("    CPX #$55	    ")
-    lines.append("    CPX $20	        ")
-    lines.append("    CPX $2233	        ")
-    lines.append("    CPY #$55	    ")
-    lines.append("    CPY $20	        ")
-    lines.append("    CPY $2233	        ")
-    lines.append("    DEA	            ")
-    lines.append("    DEC A	            ")
-    lines.append("    DEC $20	        ")
-    lines.append("    DEC $20,X	    ")
-    lines.append("    DEC $2233	        ")
-    lines.append("    DEC $2233,X	    ")
-    lines.append("    DEX	            ")
-    lines.append("    DEY	            ")
-    lines.append("    EOR #$55	    ")
-    lines.append("    EOR $20	        ")
-    lines.append("    EOR $20,X	    ")
-    lines.append("    EOR $2233	        ")
-    lines.append("    EOR $2233,X	    ")
-    lines.append("    EOR $2233,Y	    ")
-    lines.append("    EOR ($20,X)	    ")
-    lines.append("    EOR ($20),Y	    ")
-    lines.append("    EOR ($20)	    ")
-    lines.append("    INA")
-    lines.append("    INC A	            ")
-    lines.append("    INC $20	        ")
-    lines.append("    INC $20,X	    ")
-    lines.append("    INC $2233	        ")
-    lines.append("    INC $2233,X	    ")
-    lines.append("    INX	            ")
-    lines.append("    INY	            ")
-    lines.append("    JMP jmp1	        ")
-    lines.append("jmpa: dw &jmp2 ")
-    lines.append("      dw &jmp3 ")
-    lines.append("jmp1:")
-    lines.append("    JMP (jmpa)	    ")
-    lines.append("jmp2:")
-    lines.append("    ldx #$02 ")
-    lines.append("    JMP (jmpa,X)	    ")
-    lines.append("jmp3:")
-    lines.append("    JSR jsrhandler    ")
-    lines.append("    LDA #$55	    ")
-    lines.append("    LDA $20	        ")
-    lines.append("    LDA $20,X	    ")
-    lines.append("    LDA $2233	        ")
-    lines.append("    LDA $2233,X	    ")
-    lines.append("    LDA $2233,Y	    ")
-    lines.append("    LDA ($20,X)	    ")
-    lines.append("    LDA ($20),Y	    ")
-    lines.append("    LDA ($20)	    ")
-    lines.append("    LDX #$55	    ")
-    lines.append("    LDX $20	        ")
-    lines.append("    LDX $20,Y	    ")
-    lines.append("    LDX $2233	        ")
-    lines.append("    LDX $2233,Y	    ")
-    lines.append("    LDY #$55	    ")
-    lines.append("    LDY $20	        ")
-    lines.append("    LDY $20,X	    ")
-    lines.append("    LDY $2233	        ")
-    lines.append("    LDY $2233,X	    ")
-    lines.append("    LSR A	            ")
-    lines.append("    LSR $20	        ")
-    lines.append("    LSR $20,X	        ")
-    lines.append("    LSR $2233	        ")
-    lines.append("    LSR $2233,X	    ")
-    lines.append("    NOP	            ")
-    lines.append("    ORA #$55	        ")
-    lines.append("    ORA $20	        ")
-    lines.append("    ORA $20,X	        ")
-    lines.append("    ORA $2233	        ")
-    lines.append("    ORA $2233,X	    ")
-    lines.append("    ORA $2233,Y	    ")
-    lines.append("    ORA ($20,X)	    ")
-    lines.append("    ORA ($20),Y	    ")
-    lines.append("    ORA ($20)	        ")
-    lines.append("    PHA	            ")
-    lines.append("    PHP               ")
-    lines.append("    PLP               ")
-    lines.append("    PLX	            ")
-    lines.append("    PLY	            ")
-    lines.append("    ROL A	            ")
-    lines.append("    ROL $20	        ")
-    lines.append("    ROL $20,X	        ")
-    lines.append("    ROL $2233	        ")
-    lines.append("    ROL $2233,X	    ")
-    lines.append("    ROR A	            ")
-    lines.append("    ROR $20	        ")
-    lines.append("    ROR $20,X	        ")
-    lines.append("    ROR $2233	        ")
-    lines.append("    ROR $2233,X	    ")
-    #lines.append("    RTI	            ")
-    #lines.append("    RTS	            ")
-    lines.append("    SBC #$55	        ")
-    lines.append("    SBC $20 	        ")
-    lines.append("    SBC $20,X	        ")
-    lines.append("    SBC $2233	        ")
-    lines.append("    SBC $2233,X	    ")
-    lines.append("    SBC $2233,Y	    ")
-    lines.append("    SBC ($20,X)	    ")
-    lines.append("    SBC ($20),Y	    ")
-    lines.append("    SBC ($20)	        ")
-    lines.append("    SEC	            ")
-    lines.append("    SED	            ")
-    lines.append("    SEI	            ")
-    lines.append("    STA $20	        ")
-    lines.append("    STA $20,X	    ")
-    lines.append("    STA $2233	        ")
-    lines.append("    STA $2233,X	    ")
-    lines.append("    STA $2233,Y	    ")
-    lines.append("    STA ($20,X)	    ")
-    lines.append("    STA ($20),Y	    ")
-    lines.append("    STA ($20)	    ")
-    lines.append("    STX $20	        ")
-    lines.append("    STX $20,Y	    ")
-    lines.append("    STX $2233	        ")
-    lines.append("    STY $20	        ")
-    lines.append("    STY $20,X	    ")
-    lines.append("    STY $2233	        ")
-    lines.append("    STZ $20	        ")
-    lines.append("    STZ $20,X	    ")
-    lines.append("    STZ $2233	        ")
-    lines.append("    STZ $2233,X	    ")
-    lines.append("    TAX	            ")
-    lines.append("    TAY	            ")
-    lines.append("    TRB $20	        ")
-    lines.append("    TRB $2233	        ")
-    lines.append("    TSB $20	        ")
-    lines.append("    TSB $2233	        ")
-    lines.append("    TSX	            ")
-    lines.append("    TXA	            ")
-    lines.append("    TXS	            ")
-    lines.append("    TYA")
-    lines.append("; A remark")
-    lines.append("    JMP $1000 ")
-    lines.append("jsrhandler:")
-    lines.append("    nop")
-    lines.append("    nop")
-    lines.append("    rts")
-    lines.append("brkhandler: ")
-    lines.append("    NOP ")
-    lines.append("    NOP ")
-    lines.append("    RTI ")
-    
-    lines.append("    ORG $1000")
-    lines.append("start: lda #$50")
-    lines.append("       sta $5000 ; blah")
-    lines.append("       sta $25")
-    lines.append("       clc")
-    lines.append("       ROR A")
-    lines.append("       adc #%10011010")
-    lines.append("       sta %0101101000111100")
-    lines.append("       sta %00111100")
-    lines.append("       lda ($20)")
-    lines.append("       adc $10,x")
-    lines.append("middle:ldx $20,y")
-    lines.append("       adc $3000,x")
-    lines.append("       adc $3000,y")
-    lines.append("       adc ($40,x) ")
-    lines.append("       adc ($40),y")
-    lines.append("       jmp $2000")
-    lines.append("       nop")
-    lines.append("       nop")
-    lines.append("label:")
-    lines.append("       nop")
-    lines.append("       org $2000")
-    lines.append("vals:  db @10,$aa,8,$cc,$dd")
-    lines.append("       be")
-    lines.append("       dw $1020,$3040")
-    lines.append("       le")
-    lines.append("       dw $1020,$3040")
-    lines.append("       ddw $1020,$3040")
-    lines.append("       dqw $1020,$3040")
-    lines.append("       adc start")
-    lines.append("       adc ($40)")
-    lines.append("end:   bpl vals")
-    lines.append("       db $aa,$bb,$cc,$dd")
-    lines.append("       nop")
-    lines.append("       org $fffc")
-    lines.append("       db $00,$00")
+    assembly_src="""
+            org $0000
+            JMP overthere 
+bhaddr:     dw &brkhandler,$0102,&another,&labels ; insert address for brkhandler
+l8addr:     dw &land8
+overthere:	      
+            LDA bhaddr 
+            LDX 1 
+            STA $FFFE 
+            LDA bhaddr,x 
+            STA $FFFF 
+            LDA #$10 
+            ADC #$55	    
+            ADC $20	        
+            ADC $20,X	    
+            ADC $0002	        
+            ADC $0010,X	    
+            ADC $0008,Y	    
+            ADC ($20,X)	    
+            ADC ($20),Y	    
+another:    ADC ($20)	    
+            AND #$55	    
+            AND $20	        
+            AND $20,X	    
+            AND $0004	        
+labels:	        
+            AND $0010,X	    
+            AND $0012,Y	    
+            AND ($20,X)	    
+            AND ($20),Y	    
+            AND ($20)	    
+            ASL A	        
+            ASL $20	        
+            ASL $20,X	    
+            ASL $2233	        
+            ASL $2233,X	    	    
+            CLC 
+            BCC land1	    
+            CLC 
+land1:	        
+            SEC 
+            BCS land2	    
+land2:	        
+            BEQ land3	    
+land3: 
+            BIT #$55	    
+            BIT $20	    
+            BIT $20,X	    
+            BIT $2233	    
+            BIT $2233,X	    
+            BMI land4	    
+land4: 
+            BNE land5	    
+land5: 
+            BPL land6	    
+land6: 
+            BRA land7	    
+land7: 
+            LDA l8addr
+            STA $fffe
+            LDX #$01
+            LDA l8addr,x
+            STA $ffff
+            BRK	            
+            BVC land8	    
+land8: 
+            BVS land9	    
+land9: 
+            CLC	            
+            CLD	            
+            CLI	            
+            CLV	            
+            CMP #$55	    
+            CMP $20	        
+            CMP $20	        
+            CMP $2233	        
+            CMP $2233,X	    
+            CMP $2233,Y	    
+            CMP ($20,X)	    
+            CMP ($20),Y	    
+            CMP ($20)	    
+            CPX #$55	    
+            CPX $20	        
+            CPX $2233	        
+            CPY #$55	    
+            CPY $20	        
+            CPY $2233	        
+            DEA	            
+            DEC A	            
+            DEC $20	        
+            DEC $20,X	    
+            DEC $2233	        
+            DEC $2233,X	    
+            DEX	            
+            DEY	            
+            EOR #$55	    
+            EOR $20	        
+            EOR $20,X	    
+            EOR $2233	        
+            EOR $2233,X	    
+            EOR $2233,Y	    
+            EOR ($20,X)	    
+            EOR ($20),Y	    
+            EOR ($20)	    
+            INA
+            INC A	            
+            INC $20	        
+            INC $20,X	    
+            INC $2233	        
+            INC $2233,X	    
+            INX	            
+            INY	            
+            JMP jmp1	        
+jmpa:       dw &jmp2 
+            dw &jmp3 
+jmp1:
+            JMP (jmpa)	    
+jmp2:
+            ldx #$02 
+            JMP (jmpa,X)	    
+jmp3:
+            JSR jsrhandler    
+            LDA #$55	    
+            LDA $20	        
+            LDA $20,X	    
+            LDA $2233	        
+            LDA $2233,X	    
+            LDA $2233,Y	    
+            LDA ($20,X)	    
+            LDA ($20),Y	    
+            LDA ($20)	    
+            LDX #$55	    
+            LDX $20	        
+            LDX $20,Y	    
+            LDX $2233	        
+            LDX $2233,Y	    
+            LDY #$55	    
+            LDY $20	        
+            LDY $20,X	    
+            LDY $2233	        
+            LDY $2233,X	    
+            LSR A	            
+            LSR $20	        
+            LSR $20,X	        
+            LSR $2233	        
+            LSR $2233,X	    
+            NOP	            
+            ORA #$55	        
+            ORA $20	        
+            ORA $20,X	        
+            ORA $2233	        
+            ORA $2233,X	    
+            ORA $2233,Y	    
+            ORA ($20,X)	    
+            ORA ($20),Y	    
+            ORA ($20)	        
+            PHA	            
+            PHP               
+            PLP               
+            PLX	            
+            PLY	            
+            ROL A	            
+            ROL $20	        
+            ROL $20,X	        
+            ROL $2233	        
+            ROL $2233,X	    
+            ROR A	            
+            ROR $20	        
+            ROR $20,X	        
+            ROR $2233	        
+            ROR $2233,X	    
+            SBC #$55	        
+            SBC $20 	        
+            SBC $20,X	        
+            SBC $2233	        
+            SBC $2233,X	    
+            SBC $2233,Y	    
+            SBC ($20,X)	    
+            SBC ($20),Y	    
+            SBC ($20)	        
+            SEC	            
+            SED	            
+            SEI	            
+            STA $20	        
+            STA $20,X	    
+            STA $2233	        
+            STA $2233,X	    
+            STA $2233,Y	    
+            STA ($20,X)	    
+            STA ($20),Y	    
+            STA ($20)	    
+            STX $20	        
+            STX $20,Y	    
+            STX $2233	        
+            STY $20	        
+            STY $20,X	    
+            STY $2233	        
+            STZ $20	        
+            STZ $20,X	    
+            STZ $2233	        
+            STZ $2233,X	    
+            TAX	            
+            TAY	            
+            TRB $20	        
+            TRB $2233	        
+            TSB $20	        
+            TSB $2233	        
+            TSX	            
+            TXA	            
+            TXS	            
+            TYA
+; A remark
+            JMP $1000 
+jsrhandler:
+            nop
+            nop
+            rts
+brkhandler: 
+            NOP 
+            NOP 
+            RTI 
 
+    ORG $1000
+start: lda #$50
+       sta $5000 ; blah
+       sta $25
+       clc
+       ROR A
+       adc #%10011010
+       sta %0101101000111100
+       sta %00111100
+       lda ($20)
+       adc $10,x
+middle:ldx $20,y
+       adc $3000,x
+       adc $3000,y
+       adc ($40,x) 
+       adc ($40),y
+       jmp $2000
+       nop
+       nop
+label:
+       nop
+       org $2000
+vals:  db @10,$aa,8,$cc,$dd
+       be
+       dw $1020,$3040
+       le
+       dw $1020,$3040
+       ddw $1020,$3040
+       dqw $1020,$3040
+       adc start
+       adc ($40)
+end:   bpl vals
+       db $aa,$bb,$cc,$dd
+       nop
+       org $fffc
+       db $00,$00
+"""
+    lines = assembly_src.splitlines()
 
     # Instantiate the assembler, assemble the code, grab the object_code
     a = asm6502.asm6502(debug=debug)
