1ac1b6fc665073198c95165de3ad5c367a7135e6
djohnston <david.johnston@analogueinsight.com>
2026-07-18 20:59:50 -0700

    setup.py so it can be imported as a package

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="py6502",
+    version="1.0.0",
+    description="A 6502 assembler, dissasembler and simulator",
+    py_modules=["asm6502","dis6502","sim6502"],
+)
+
